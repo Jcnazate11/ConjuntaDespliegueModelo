dd7d83e33cc570e2a5df218c93de1c17191bc0f1
Jeanhela Nazate <jcnazate@espe.edu.ec>
2024-07-24 04:29:56 -0500

    Flask

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+import sys
+import io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
+
 # app.py
 from flask import Flask, request, jsonify, send_from_directory
 import joblib
